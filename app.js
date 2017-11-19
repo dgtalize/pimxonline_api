2952cd2c49bf1723ad0819a765bcbb59e7639eec
DGtalize Admin <admin@dgtalize.com>
2017-11-19 16:39:31 -0300

    HTTP headers setup

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// headers setup
+app.all('/*', function (req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // Set custom headers for CORS
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 // routes
 require('./api/routes/index')(app, {});
 
